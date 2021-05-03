25721c3acbe533c1d5da439c5b1c3ee5f93a40a7
David Jegat <david.jegat@knplabs.com>
2021-05-03 15:46:37 +0200

    Add doc

--- docjs/variables.js
+++ docjs/variables.js
@@ -1,4 +1,3 @@
-import { unmountComponentAtNode } from 'react-native-web';
 /**
  * Une variable est une donnée enregistré en mémoire. Elle possède
  * un identifiant qui permet d'accéder à la donnée.
@@ -78,3 +77,69 @@ for (let index in names) {
 for (let name of names) {
     console.log(name); // Afficher "Jean", puis "jack", puis "jane"
 }
+
+/**
+ * 2. Les objects
+ */
+let book = { title: 'Retour de vacance', author: 'Jean', price: 24.5 }
+
+// Accéde au titre de mon bouquin
+console.log(book.title) // "Retour de vacance"
+
+// On peut accéder à une valeur en utilisant une chaine de character
+console.log(book['price']) // 24.5
+
+// Ce que ça nous permet de faire:
+let key = 'author'
+console.log(book[key]) // "Jean"
+
+// un éxemple plus complèxe
+const library = [
+    { title: 'Les murailles de feu' },
+    { title: 'Le philosophe' },
+    { title: 'JS pour les nuls' },
+]
+
+console.log(library[1].title)
+
+/**
+ * Les objets peuvent être imbriquer les uns dans les autres
+ */
+const animal = { type: 'chat' }
+const persan = { age: 32, name: 'Felix' }
+
+// Fusion de 2 objets
+const felix = { ...animal, ...persan }
+
+console.log(felix.type) // 'chat'
+console.log(felix.age) // 32
+console.log(felix.name) // "Felix"
+
+/**
+ * Depuis ES6 il éxsiste un system qu'on nomme la
+ * "destructuration des arguments"
+ * 
+ * Cela nous permet d'accéder facilement à des index de tableaux
+ * ou bien des clefs d'objet
+ */
+
+// Ici on déclare un tableaux
+const ages = [ 12, 34, 43, 52 ]
+
+// je vais accéder à la première valeur de mon tableaux
+// et construire un tableaux avec le reste des valeurs
+const [firstAge, ...restAges] = ages;
+
+console.log(firstAge) // 12
+console.log(restAges) // [ 34, 43, 52 ]
+
+// Ici on déclare un objet
+const product = { name: 'Casquette', price: 34.50, category: 'Cloth' }
+
+// Je vais accéder à ma clefs "name" et créer un nouvelle object
+// avec le reste des clefs
+const {name, ...restProduct} = product;
+
+console.log(name) // "Casquette"
+console.log(restProduct) // { price: 34.50, category: 'Cloth' } 
+
