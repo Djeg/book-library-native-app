ce0f1dad82079307a268de9a86f956b0f3a7d14d
David Jegat <david.jegat@knplabs.com>
2021-05-04 15:01:05 +0200

    Stylesheet example

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-const styles = StyleSheet.create({
+const styleSheet = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
@@ -14,24 +14,27 @@ const styles = StyleSheet.create({
   },
   red: {
     color: 'red',
+  },
+  bigText: {
+    fontSize: '20px',
   }
 });
 
 const Hello = ({ name, age, style }) => {
   if (age >= 18) {
-    return <Text style={[styles.helloText, style]}>Bonjour {name}, Vous êtes majeur</Text>
+    return <Text style={[styleSheet.helloText, style]}>Bonjour {name}, Vous êtes majeur</Text>
   }
 
-  return <Text style={[styles.helloText, style]}>Bonjour {name}, Vous n'êtes pas majeur</Text>
+  return <Text style={[styleSheet.helloText, style]}>Bonjour {name}, Vous n'êtes pas majeur</Text>
 }
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={styleSheet.container}>
       <Hello name="David" age="30" />
       <Hello name="Pierre" age="12" />
-      <Hello style={styles.red} name="Paul" age="32" />
-      <Hello name="Jack" age="16" />
+      <Hello style={styleSheet.red} name="Paul" age="32" />
+      <Hello style={styleSheet.bigText} name="Jack" age="16" />
       <Hello name="David" age="30" />
       <Hello name="Pierre" age="12" />
       <Hello name="Paul" age="32" />
