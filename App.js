ed9aaa53d1c2517d0816cba0c2a7b0e8e18db62b
David Jegat <david.jegat@knplabs.com>
2021-05-04 11:31:26 +0200

    Update exo

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, ScrollView } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Hello = () => {
+  return <Text>Bonjour tout le monde</Text>
+}
 
 export default function App() {
   return (
-    <ScrollView style={styles.container}>
-      <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et ex mauris. Nulla pharetra mi at dictum iaculis. Nullam vitae euismod eros. Nullam ac ipsum sapien. Suspendisse felis elit, laoreet vel rutrum ut, aliquam sit amet elit. Fusce mattis mauris nec est rhoncus blandit. Donec consectetur, dui sed aliquam luctus, ipsum metus sollicitudin magna, ac viverra justo libero eu enim. Maecenas fermentum, lectus id cursus dictum, ipsum sapien tristique lectus, at suscipit libero nunc sit amet felis. Quisque faucibus sollicitudin nulla.</Text>
-      <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et ex mauris. Nulla pharetra mi at dictum iaculis. Nullam vitae euismod eros. Nullam ac ipsum sapien. Suspendisse felis elit, laoreet vel rutrum ut, aliquam sit amet elit. Fusce mattis mauris nec est rhoncus blandit. Donec consectetur, dui sed aliquam luctus, ipsum metus sollicitudin magna, ac viverra justo libero eu enim. Maecenas fermentum, lectus id cursus dictum, ipsum sapien tristique lectus, at suscipit libero nunc sit amet felis. Quisque faucibus sollicitudin nulla.</Text>
-      <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et ex mauris. Nulla pharetra mi at dictum iaculis. Nullam vitae euismod eros. Nullam ac ipsum sapien. Suspendisse felis elit, laoreet vel rutrum ut, aliquam sit amet elit. Fusce mattis mauris nec est rhoncus blandit. Donec consectetur, dui sed aliquam luctus, ipsum metus sollicitudin magna, ac viverra justo libero eu enim. Maecenas fermentum, lectus id cursus dictum, ipsum sapien tristique lectus, at suscipit libero nunc sit amet felis. Quisque faucibus sollicitudin nulla.</Text>
-      <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et ex mauris. Nulla pharetra mi at dictum iaculis. Nullam vitae euismod eros. Nullam ac ipsum sapien. Suspendisse felis elit, laoreet vel rutrum ut, aliquam sit amet elit. Fusce mattis mauris nec est rhoncus blandit. Donec consectetur, dui sed aliquam luctus, ipsum metus sollicitudin magna, ac viverra justo libero eu enim. Maecenas fermentum, lectus id cursus dictum, ipsum sapien tristique lectus, at suscipit libero nunc sit amet felis. Quisque faucibus sollicitudin nulla.</Text>
-      <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et ex mauris. Nulla pharetra mi at dictum iaculis. Nullam vitae euismod eros. Nullam ac ipsum sapien. Suspendisse felis elit, laoreet vel rutrum ut, aliquam sit amet elit. Fusce mattis mauris nec est rhoncus blandit. Donec consectetur, dui sed aliquam luctus, ipsum metus sollicitudin magna, ac viverra justo libero eu enim. Maecenas fermentum, lectus id cursus dictum, ipsum sapien tristique lectus, at suscipit libero nunc sit amet felis. Quisque faucibus sollicitudin nulla.</Text>
+    <View style={styles.container}>
+      <Hello />
       <StatusBar style="auto" />
-    </ScrollView>
+    </View>
   );
 }
 
@@ -19,5 +19,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
 });
