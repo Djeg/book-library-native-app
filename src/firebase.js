9787450ef4140e7c1d8f8a4ef57d6799552d5dd2
David Jegat <david.jegat@gmail.com>
2021-09-07 16:59:38 +0200

    Ajout de firebase

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyAj8a1jWv_K5MCcjkmaYVVBWayaRIb1RJE',
+  authDomain: 'book-library-c0f88.firebaseapp.com',
+  projectId: 'book-library-c0f88',
+  storageBucket: 'book-library-c0f88.appspot.com',
+  messagingSenderId: '461014580063',
+  appId: '1:461014580063:web:a81cf3555badf0ef7c94ea',
+  measurementId: 'G-NX3KF8EYPZ',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+// On exporte la base de données firestore
+export default getFirestore()
--- src/screens/BookList.js
+++ src/screens/BookList.js
@@ -1,40 +1,72 @@
-import React from 'react'
+import { collection, getDocs } from 'firebase/firestore'
+import React, { useEffect, useState } from 'react'
 import { ScrollView } from 'react-native'
 import BookThumbnail from '../BookThumbnail'
+import db from '../firebase'
 
 export default () => {
+  const [books, setBooks] = useState([])
+
+  // Nous créons un effét qui se déclenchera lors de l'affichage
+  // de notre composant
+  useEffect(() => {
+    // Je céer une fonction asynchrone afin de récupérer
+    // les livres de firestore
+    const fetchBooks = async () => {
+      // On utilise getDocs afin de récupérer plusieurs documents
+      // On utilise collection afin de récupérer une collection
+      // getDocs est asynchrone, et retourne un QuerySnaphot
+      const snapshot = await getDocs(collection(db, 'books'))
+
+      // Dans ce query snapshot, nous avons la liste des document
+      // snapshot.docs (contient un tableau de documents)
+      // Une fois que l'on obtient un document, nous pouvons
+      // récupérer les données graçe à document.data()
+      const receivedBooks = snapshot.docs.map((document) => {
+        // Ici, document.data() retourne les données stocké
+        // dans le document concerné. Attention, document.data
+        // ne retourne pas l'id !
+        // Pour obtenir il faut utiliser document.id
+        return {
+          ...document.data(),
+          id: document.id,
+        }
+      })
+
+      // On assigne notre state "books" avec les livres recus
+      // (receivedBooks)
+      setBooks(receivedBooks)
+    }
+
+    // j'appel la fonction asynchrone
+    fetchBooks()
+  }, [])
+
+  // Voici un exemple, plus conscis et difficile d'utilisation
+  // des promesses dans useEffect
+  //
+  // useEffect(() => {
+  //   getDocs(collection(db, 'books'))
+  //     .then(({ docs }) =>
+  //       docs.map((document) => ({
+  //         ...document.data(),
+  //         id: document.id,
+  //       }))
+  //     )
+  //     .then(setBooks)
+  // }, [])
+
   return (
     <ScrollView>
-      <BookThumbnail
-        title='Harry Potter'
-        description='Harry Potter'
-        price={21.0}
-        image='https://static.dezeen.com/uploads/2021/04/harry-potter-michele-de-lucchi-covers_dezeen_2364_col_21-852x1261.jpg'
-      />
-      <BookThumbnail
-        title='Harry Potter'
-        description='Harry Potter'
-        price={21.0}
-        image='https://static.dezeen.com/uploads/2021/04/harry-potter-michele-de-lucchi-covers_dezeen_2364_col_21-852x1261.jpg'
-      />
-      <BookThumbnail
-        title='Harry Potter'
-        description='Harry Potter'
-        price={21.0}
-        image='https://static.dezeen.com/uploads/2021/04/harry-potter-michele-de-lucchi-covers_dezeen_2364_col_21-852x1261.jpg'
-      />
-      <BookThumbnail
-        title='Harry Potter'
-        description='Harry Potter'
-        price={21.0}
-        image='https://static.dezeen.com/uploads/2021/04/harry-potter-michele-de-lucchi-covers_dezeen_2364_col_21-852x1261.jpg'
-      />
-      <BookThumbnail
-        title='Harry Potter'
-        description='Harry Potter'
-        price={21.0}
-        image='https://static.dezeen.com/uploads/2021/04/harry-potter-michele-de-lucchi-covers_dezeen_2364_col_21-852x1261.jpg'
-      />
+      {books.map((book) => (
+        <BookThumbnail
+          key={`book-${book.id}`}
+          title={book.title}
+          description={book.description}
+          price={book.price}
+          image={book.image}
+        />
+      ))}
     </ScrollView>
   )
 }
