aa0c2b19591c05616fe173fc261ad664c90eed60
David Jegat <david.jegat@gmail.com>
2021-09-08 15:15:08 +0200

    Inscription - Première tentative (pas top top)

--- src/screens/CreateAccount.js
+++ src/screens/CreateAccount.js
@@ -1,23 +1,101 @@
+import { createUserWithEmailAndPassword } from '@firebase/auth'
 import React, { useState, useEffect } from 'react'
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
 import { auth } from '../firebase'
 
 export default () => {
   const [email, setEmail] = useState('')
-  const [emailError, setEmailError] = useState(false)
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
+  const [errors, setErrors] = useState([])
+
+  // Helper Functions
+  const addError = errorMessage => {
+    // Si l'erreur existe dèja je ne fais rien
+    if (errors.includes(errorMessage)) {
+      return
+    }
+
+    // Sinon, je rajoute le message d'erreur
+    // dans mon tableaux d'erreur
+    setErrors([...errors, errorMessage])
+  }
+
+  const removeError = errorMessage => {
+    // Si l'erreur n'existe pas on ne fais rien
+    if (!errors.includes(errorMessage)) {
+      return
+    }
+
+    // Sinon, supprimer le message d'erreur
+    setErrors(errors.filter(e => e !== errorMessage))
+  }
 
   // Side Effect
+  //const [emailError, setEmailError] = useState(false)
+  //useEffect(() => {
+  //  if (!email) {
+  //    setEmailError(false)
+  //  } else if (/^[^ @]+@[^ @]+\.[a-zA-Z0-9]+$/.test(email)) {
+  //    setEmailError(false)
+  //  } else {
+  //    setEmailError(true)
+  //  }
+  //}, [email])
+
+  // La vérification de l'email
   useEffect(() => {
-    if (!email) {
-      setEmailError(false)
-    } else if (/^[^ @]+@[^ @]+\.[a-zA-Z0-9]+$/.test(email)) {
-      setEmailError(false)
-    } else {
-      setEmailError(true)
+    // Le message d'erreur
+    const message = 'Invalide email'
+
+    // Si je n'ai pas d'email ou que l'email est valide,
+    // je supprime l'erreur et arréte la fonction
+    if (!email || /^[^ @]+@[^ @]+\.[a-zA-Z0-9]+$/.test(email)) {
+      removeError(message)
+
+      return
     }
+
+    // Sinon, j'ajoute l'erreur
+    addError(message)
   }, [email])
+
+  // Actions
+
+  // Action éxécuté lorsque l'on appuie sur le bouton
+  // 's'inscrire'
+  const onPress = async () => {
+    const noPasswordMessage =
+      'Vous devez spécifier un mot de passe et le confirmer'
+    const noIdenticPasswordMessage =
+      'Vos deux mots de passe doivent être les même'
+
+    // Vérifier les mots de passe vide
+    if (!password || !confirmPassword) {
+      addError(noPasswordMessage)
+    } else {
+      removeError(noPasswordMessage)
+    }
+
+    // Vérification de l'identité des mots de passe
+    if (password && confirmPassword && password !== confirmPassword) {
+      addError(noIdenticPasswordMessage)
+    } else {
+      removeError(noIdenticPasswordMessage)
+    }
+
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      )
+
+      console.warn(userCredential)
+    } catch (e) {
+      addError(e.message)
+    }
+  }
 
   // g x => 1 + x
   // f x => g(x)
@@ -36,8 +114,6 @@ export default () => {
           style={styles.textInput}
           onChangeText={setEmail}
         />
-        <Text>Email : {email}</Text>
-        {emailError ? <Text>Email invalide</Text> : null}
       </View>
       <View>
         <Text>Votre mot de passe :</Text>
@@ -55,13 +131,13 @@ export default () => {
           onChangeText={setConfirmPassword}
         />
       </View>
+      <View>
+        {errors.map(error => (
+          <Text style={styles.error}>{error}</Text>
+        ))}
+      </View>
       <View style={styles.button}>
-        <Button
-          title="S'inscrire"
-          onPress={() => {
-            console.log(email, password, confirmPassword)
-          }}
-        />
+        <Button title="S'inscrire" onPress={onPress} />
       </View>
     </View>
   )
@@ -83,5 +159,10 @@ export const styles = StyleSheet.create({
     display: 'block',
     marginTop: 8,
     marginBottom: 10,
+  },
+  error: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: 'red',
   },
 })
